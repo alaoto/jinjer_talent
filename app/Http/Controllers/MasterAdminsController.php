32e7e2d90128ca884cf4250824a93b7493b934e4
alaoto <tatsuki.aoto@alnetz.co.jp>
2024-04-09 15:54:48 +0900

    登録APIを作成した。

--- app/Http/Controllers/MasterAdminsController.php
+++ app/Http/Controllers/MasterAdminsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\MasterAdmins;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
@@ -10,10 +11,10 @@ class MasterAdminsController extends Controller
     public function readMasterAdmins()
     {
         try {
-        // MasterAdminモデルを使用してすべての管理者を取得します
-        $admins = MasterAdmins::all();
+            // MasterAdminモデルを使用してすべての管理者を取得します
+            $admins = MasterAdmins::all();
 
-        // JSON形式でデータを返す
+            // JSON形式でデータを返す
             return response()->json($admins, 200);
         } catch (\Exception $e) {
             // エラーメッセージをログに記録
@@ -21,6 +22,28 @@ class MasterAdminsController extends Controller
 
             // 403エラーを返す
             return response()->json(['error' => 'Failed to read master admins'], 403);
+        }
+    }
+
+    public function createMasterAdmin(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'admin_name' => 'required',
+                'password' => 'required',
+            ]);
+
+            $validatedData['password'] = Hash::make($request->password);
+
+            $masterAdmin = MasterAdmins::create($validatedData);
+
+            return response()->json(['message' => 'Master admin created successfully', 'data' => $masterAdmin], 200);
+        } catch (\Exception $e) {
+            // エラーメッセージをログに記録
+            Log::error($e->getMessage());
+
+            // 403エラーを返す
+            return response()->json(['error' => 'Failed to create master admin'], 403);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/masteradmins', [MasterAdminsController::class, 'readMasterAdmins']);
+Route::post('/masteradmins', [MasterAdminsController::class, 'createMasterAdmin']);
